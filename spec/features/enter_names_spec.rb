c362d828d08c2bf662a1de0de827aa62983730fe
sus111 <sgraham535@gmail.com>
2016-07-19 15:51:50 +0100

    add first test

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,10 +1,10 @@
-# 
-# feature 'Enter names' do
-#   scenario 'submitting names' do
-#     visit('/')
-#     fill_in :player_one_name, with: 'John'
-#     fill_in :player_two_name, with: 'Sarah'
-#     click_button('Submit')
-#     expect(page).to have_content 'John vs Sarah'
-# end
-# end
+
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :player_one_name, with: 'John'
+    fill_in :player_two_name, with: 'Sarah'
+    click_button('Submit')
+    expect(page).to have_content 'John vs Sarah'
+end
+end
