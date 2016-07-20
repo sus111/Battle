4180c92f3198ee6a94635e271e0b3c60fde56ed3
sus111 <sgraham535@gmail.com>
2016-07-20 10:18:32 +0100

    ex 18

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,10 +1,12 @@
+require 'spec_helper'
+require '../battle/app'
 
 feature 'Enter names' do
   scenario 'submitting names' do
     visit('/')
-    fill_in :player_one_name, with: 'John'
-    fill_in :player_two_name, with: 'Sarah'
+    fill_in :player_1_name, with: 'Sarah'
+    fill_in :player_2_name, with: 'Dave'
     click_button('Submit')
-    expect(page).to have_content 'John vs Sarah'
+    expect(page).to have_content 'Sarah vs. Dave'
 end
 end
